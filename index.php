77a035a97a97b6feed08afab95f82cafdf294bce
unknown <sampv67@gmail.com>
2018-01-30 20:27:52 +0000

    First commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<?php 
+include thera.php;
+?>
+</head>
+
+<body>
+THIS IS A TEST
+
+
+
+
+</body>
+
+</html>
---
+++ thera.php
@@ -0,0 +1,51 @@
+<?php 
+//get thera JSON data from eve-scout and parse into DB
+//checks last modified to figure out if new data needs to be added to DB
+function theraJson()
+{
+	
+}
+
+
+//retrieve JSON from eve-scout and save as session object?
+function getThera()
+{
+	
+}
+
+
+//get data from DB and return as JSON (probably no longer needed)
+function parThera()
+{
+	
+}
+
+//takes form data, converts to system ID and passes to getJumps to query API
+function distCalc()
+{
+	
+}
+
+//queries the ESI api to get the number of jumps between two system ID
+function getJumps($origin,$dest)
+{
+	
+	
+}
+
+//query DB to return sys ID from name provided
+function getsysID($find)
+{
+	
+}
+
+//query DB to return sys name from ID provided
+function getsysNam($find)
+{
+	
+}
+
+
+
+
+?>
